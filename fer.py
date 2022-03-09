36191eaa59e4d8e836e2ea842deaca9d359543f5
Emin Tagiev <emin.tagiev@phystech.edu>
2022-03-09 15:39:08 +0300

    Implement inference through batches for the list of images

--- fer.py
+++ fer.py
@@ -1,10 +1,17 @@
+import os
+
+import json
 import cv2
 import numpy as np
+import pandas as pd
 import torch
 import torchvision.transforms as transforms
 from facenet_pytorch import MTCNN
 from PIL import Image
+from torch.utils.data import DataLoader, Dataset
+from tqdm import tqdm
 
+from augmentations import get_transforms
 from config import CFG
 from model import FERModel
 from pre_trained_models import get_pretrained_model
@@ -24,11 +31,42 @@ fer_transforms = transforms.Compose(
 )
 
 
+class FERInferenceDataset(Dataset):
+    def __init__(self, dict_with_boxes, transform=None):
+        self.df = pd.DataFrame.from_dict(dict_with_boxes)
+        self.df_without_nans = self.df[~self.df.isnull().any(axis=1)]
+        self.image_paths = self.df_without_nans["image_paths"].values
+        self.x = self.df_without_nans["x"].values
+        self.y = self.df_without_nans["y"].values
+        self.w = self.df_without_nans["w"].values
+        self.h = self.df_without_nans["h"].values
+        self.transform = transform
+
+    def __len__(self):
+        return self.df_without_nans.shape[0]
+
+    def __getitem__(self, idx):
+        file_path = self.image_paths[idx]
+        file_name = os.path.basename(file_path)
+        x = self.x[idx]
+        y = self.y[idx]
+        w = self.w[idx]
+        h = self.h[idx]
+        # Read image
+        image = cv2.imread(file_path)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        image = image[int(y) : int(h), int(x) : int(w)]
+        if self.transform:
+            augmented = self.transform(image=image)
+            image = augmented["image"]
+        return {"face_coordinates": np.array([x, y, w, h]), "image_name": file_name, "image": image}
+
+
 class FER:
     def __init__(self):
         self.device = torch.device(f"cuda" if torch.cuda.is_available() else "cpu")
         self.model = None
-        self.mtcnn = MTCNN(select_largest=True, device=self.device)
+        self.mtcnn = MTCNN(keep_all=True, select_largest=True, device=self.device)
 
     def get_pretrained_model(self, model_name):
         self.model = get_pretrained_model(model_name)
@@ -42,12 +80,14 @@ class FER:
         self.model.eval()
 
     def predict_image(self, path_to_image, show_top=False, save_result=True):
+        if not os.path.isfile(path_to_image):
+            FileNotFoundError("File not found!")
         result_list = []
         frame = cv2.imread(path_to_image)
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         # Detect faces
         boxes, _ = self.mtcnn.detect(frame, landmarks=False)
-        if boxes.any():
+        if boxes is not None:
             for (x, y, w, h) in boxes:
                 image = gray[int(y) : int(h), int(x) : int(w)]
                 # Convert to PIL image
@@ -99,8 +139,93 @@ class FER:
             print("No faces detected!")
         return result_list
 
-    def predict_list_images(self, path_to_folder):
-        pass
+    def detect_faces(self, path_to_folder):
+        image_list = os.listdir(path_to_folder)
+        result_dict = {}
+        image_paths = []
+        x_list = []
+        y_list = []
+        w_list = []
+        h_list = []
+        for image_name in tqdm(image_list):
+            # Load image
+            image_path = os.path.join(path_to_folder, image_name)
+            image = cv2.imread(image_path)
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            # When batch is full, detect faces and reset batch list
+            boxes, _ = self.mtcnn.detect(image, landmarks=False)
+            image_paths.append(image_path)
+            if boxes is not None:
+                box = boxes[0]
+                x_list.append(box[0])
+                y_list.append(box[1])
+                w_list.append(box[2])
+                h_list.append(box[3])
+            else:
+                x_list.append(np.nan)
+                y_list.append(np.nan)
+                w_list.append(np.nan)
+                h_list.append(np.nan)
+        result_dict["image_paths"] = image_paths
+        result_dict["x"] = x_list
+        result_dict["y"] = y_list
+        result_dict["w"] = w_list
+        result_dict["h"] = h_list
+        return result_dict
+
+    def predict_list_images(self, path_to_folder, batch_size):
+        result_dict = {}
+        dict_with_boxes = self.detect_faces(path_to_folder)
+        print(dict_with_boxes)
+        inference_dataset = FERInferenceDataset(
+            dict_with_boxes=dict_with_boxes, transform=get_transforms(data="valid")
+        )
+        inference_loader = DataLoader(
+            inference_dataset,
+            batch_size=batch_size,
+            shuffle=False,
+            drop_last=False,
+        )
+        image_names = []
+        box_coordinates = []
+        pred_probs = []
+        # Inference loop
+        for inference_batch in tqdm(inference_loader):
+            # Append image names
+            names = inference_batch["image_name"]
+            image_names.append(names)
+            print(image_names)
+            # Append box coordinates
+            face_coordinates = inference_batch["face_coordinates"]
+            box_coordinates.append(face_coordinates)
+            print(box_coordinates)
+            # Inference on the batch
+            images = inference_batch["image"]
+            images = images.to(self.device)
+            with torch.no_grad():
+                y_preds = self.model(images)
+            pred_probs.append(y_preds.softmax(1).to("cpu").numpy())
+        # Concatenate output arrays
+        image_names = np.concatenate(image_names)
+        box_coordinates = np.concatenate(box_coordinates)
+        pred_probs = np.concatenate(pred_probs)
+        # Get prediction classes
+        emotion_index_array = pred_probs.argmax(1)
+        emotion_class_array = np.array([emotion_dict[idx] for idx in emotion_index_array])
+        # Get max probabilty values
+        max_prob_array = pred_probs.max(axis=1)
+        # Fill the result dictionary
+        result_dict["image_name"] = image_names.tolist()
+        result_dict["box_coordinate"] = box_coordinates.tolist()
+        result_dict["emotion"] = emotion_class_array.tolist()
+        result_dict["probability"] = max_prob_array.tolist()
+        # Convert dictionary of lists to the list of dictionaries to make it convenient in json format
+        result_list = [dict(zip(result_dict, t)) for t in zip(*result_dict.values())]
+        # Write json with results
+        result_json = json.dumps(result_list, indent=4)
+        with open('result.json', 'w') as f:
+            f.write(result_json)
+        return result_dict
 
     def analyze_video(self, path_to_video):
         pass
--- test.py
+++ test.py
@@ -2,5 +2,6 @@ from fer import FER
 
 fer = FER()
 fer.get_pretrained_model("resnet34_best")
-result = fer.predict_image("/home/emin/Desktop/udacha.jpg", show_top=True, save_result=True)
+result = fer.predict_image("/home/emin/Desktop/168.png", show_top=True, save_result=True)
 print(result)
+result_dict = fer.predict_list_images(path_to_folder='./test_images', batch_size=2)
