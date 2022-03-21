e052b70380a8bd0d77b24a3a2b8b30daa96bbf83
Emin Tagiev <emin.tagiev@phystech.edu>
2022-03-21 18:10:59 +0300

    Write tests for predict_list_images method from FER class

--- test.py
+++ test.py
@@ -12,27 +12,15 @@ print(result)
 result = fer.predict_image(frame, show_top=True)
 print(result)
 
-frame = cv2.imread("./test_images/no_face.png")
-result = fer.predict_image(frame)
-print(result)
-
-frame = cv2.imread("/home/emin/Desktop/Dollarphotoclub_71896210.jpg")
-result = fer.predict_image(frame)
-print(result)
-result = fer.predict_image(frame, show_top=True)
-print(result)
-
 result_json_list = fer.predict_list_images(
     path_to_input="./test_images/", path_to_output="./output_images", save_images=True
 )
-print(result_json_list)
-
-fer.analyze_video(path_to_video="./test_video.mp4", video_name="output_video.mp4")
-
-df = FER.json_to_pandas("./test_video/result.json")
+df = FER.json_to_pandas("./output_images/result.json")
 print(df.head())
 
-fer.analyze_video(path_to_video="./test_video_2.mp4", video_name="output_video.mp4")
+fer.analyze_video(path_to_video="./test_video.mp4", video_name="output_video.mp4")
+df = FER.json_to_pandas("./test_video/result.json")
+print(df.head())
 
 result_dict = fer.test_fer()
 print(result_dict)
