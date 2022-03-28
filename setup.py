c6857515358d941f459e33f678a6c0f162e6a235
Emin Tagiev <emin.tagiev@phystech.edu>
2022-03-28 16:23:25 +0300

    Add setup file and file for setting the version of the package

---
+++ setup.py
@@ -0,0 +1,61 @@
+from pathlib import Path
+from typing import List
+
+from setuptools import find_packages, setup
+
+# Package meta-data.
+NAME = "fer-pytorch"
+DESCRIPTION = "Facial Expression Recognition package, implemented in Pytorch."
+URL = "https://github.com/Emilien-mipt/fer-pytorch"
+EMAIL = "emin.tagiev@phystech.edu"
+AUTHOR = "Emin Tagiev"
+REQUIRES_PYTHON = ">=3.6.0"
+
+long_description = DESCRIPTION
+
+# Load the package's VERSION file as a dictionary.
+about = {}
+ROOT_DIR = Path(__file__).resolve().parent
+REQUIREMENTS_DIR = ROOT_DIR / "requirements"
+PACKAGE_DIR = ROOT_DIR / "fer_pytorch"
+
+with open(PACKAGE_DIR / "VERSION") as f:
+    _version = f.read().strip()
+    about["__version__"] = _version
+
+
+def list_reqs(fname: str = "requirements.txt") -> List[str]:
+    with open(REQUIREMENTS_DIR / fname) as fd:
+        return fd.read().splitlines()
+
+
+setup(
+    name=NAME,
+    version=about["__version__"],
+    description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author=AUTHOR,
+    author_email=EMAIL,
+    python_requires=REQUIRES_PYTHON,
+    url=URL,
+    packages=find_packages(exclude=("tests",)),
+    package_data={"fer_pytorch": ["VERSION"]},
+    install_requires=list_reqs(),
+    extras_require={},
+    include_package_data=True,
+    license="BSD-3",
+    classifiers=[
+        # Trove classifiers
+        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+    ],
+)
