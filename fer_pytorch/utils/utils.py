d82e9ccef2285d2b9d2027a292b5b7f0f5335321
Emin Tagiev <emin.tagiev@phystech.edu>
2022-03-29 23:03:45 +0300

    Fix utils annotation

--- fer_pytorch/utils/utils.py
+++ fer_pytorch/utils/utils.py
@@ -57,7 +57,7 @@ class AverageMeter:
         self.sum = 0.0
         self.count = 0.0
 
-    def update(self, val: float, n: int = 1) -> None:
+    def update(self, val: Any, n: int = 1) -> None:
         self.val = val
         self.sum += val * n
         self.count += n
