9996ab657d9ada4bcbb7aa4828e90a6dc930b54b
Emin Tagiev <emin.tagiev@phystech.edu>
2022-05-13 15:29:17 +0300

    Parametrize tests for image prediction

--- tests/test_predict_image.py
+++ tests/test_predict_image.py
@@ -1,46 +1,37 @@
 import cv2
 import numpy as np
-
-from fer_pytorch.fer import FER
+import pytest
 
 PATH_HAPPY = "tests/test_images/happy.jpg"
 PATH_SURPRIZE = "tests/test_images/surprize.jpg"
 
 PATH_NOFACE = "tests/test_images/no_face.jpg"
 
-fer = FER()
-fer.get_pretrained_model(model_name="resnet34")
 
-
-def test_with_face_types():
+@pytest.mark.parametrize("fer", ["resnet34_fer", "mobilenet_fer"], indirect=True)
+@pytest.mark.parametrize("show_top", ["True", "False"])
+def test_with_face_types(fer, show_top):
     input = cv2.imread(PATH_HAPPY)
 
     # Test full output
-    result = fer.predict_image(input)
+    result = fer.predict_image(input, show_top=show_top)
     assert isinstance(result, list)
     assert len(result) == 1
 
     result_dict = result[0]
     assert isinstance(result_dict, dict)
 
-    assert ("box" in result_dict.keys()) and ("emotions" in result_dict.keys())
+    if show_top is False:
+        assert ("box" in result_dict.keys()) and ("emotions" in result_dict.keys())
+        assert isinstance(result_dict["emotions"], dict)
+    else:
+        assert ("box" in result_dict.keys()) and ("top_emotion" in result_dict.keys())
+        assert isinstance(result_dict["top_emotion"], dict)
     assert isinstance(result_dict["box"], list)
-    assert isinstance(result_dict["emotions"], dict)
-
-    # Test top output
-    result_top = fer.predict_image(input, show_top=True)
-    assert isinstance(result_top, list)
-    assert len(result_top) == 1
-
-    result_dict_top = result_top[0]
-    assert isinstance(result_dict_top, dict)
-
-    assert ("box" in result_dict_top.keys()) and ("top_emotion" in result_dict_top.keys())
-    assert isinstance(result_dict_top["box"], list)
-    assert isinstance(result_dict_top["top_emotion"], dict)
 
 
-def test_no_face():
+@pytest.mark.parametrize("fer", ["resnet34_fer", "mobilenet_fer"], indirect=True)
+def test_no_face(fer):
     no_face = cv2.imread(PATH_NOFACE)
     result_no_face = fer.predict_image(no_face)
 
@@ -48,7 +39,8 @@ def test_no_face():
     assert len(result_no_face) == 0
 
 
-def test_happy_values():
+@pytest.mark.parametrize("fer", ["resnet34_fer", "mobilenet_fer"], indirect=True)
+def test_happy_values(fer):
     input = cv2.imread(PATH_HAPPY)
 
     result_dict = fer.predict_image(input)[0]
@@ -66,7 +58,7 @@ def test_happy_values():
         and ("disgust" in emotion_dict.keys())
         and ("fear" in emotion_dict.keys())
     )
-    assert emotion_dict["happiness"] > 0.95
+    assert emotion_dict["happiness"] > 0.8
 
     sum_probs = 0
     for value in emotion_dict.values():
@@ -75,7 +67,8 @@ def test_happy_values():
     np.testing.assert_almost_equal(1.0, sum_probs, decimal=3)
 
 
-def test_surprize():
+@pytest.mark.parametrize("fer", ["resnet34_fer", "mobilenet_fer"], indirect=True)
+def test_surprize(fer):
     input = cv2.imread(PATH_SURPRIZE)
 
     result_dict = fer.predict_image(input)[0]
@@ -93,7 +86,7 @@ def test_surprize():
         and ("disgust" in emotion_dict.keys())
         and ("fear" in emotion_dict.keys())
     )
-    assert emotion_dict["surprise"] > 0.9
+    assert emotion_dict["surprise"] > 0.8
 
     sum_probs = 0
     for value in emotion_dict.values():
